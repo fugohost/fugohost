eef9f94f2b425ea811b3f36e7d236e5cf9047e2d
fugo host <apoorva.rao@fugohost.com>
2023-10-15 17:06:23 -0400

    just a heading

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello Everyone, Welcome to Fugohost</h2>
+      <h2>Hello Everyone, Welcome to Fugohost!</h2>
     </div>
   );
 }
