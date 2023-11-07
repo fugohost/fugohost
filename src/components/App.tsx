73c97baa5d9238261e959518ca06f2a86cb67260
fugo host <apoorva.rao@fugohost.com>
2023-11-06 20:49:23 -0500

    google analytics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,11 @@ import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import 'bootstrap/dist/css/bootstrap.css';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-TCX2K37CPJ"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
   return (
     <div className="App">
